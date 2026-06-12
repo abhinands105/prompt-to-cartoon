543eec2d57e9e227a6cf4e2e46a73f664b16fd43
Abhinand S <abhiske71@gmail.com>
2026-06-12 07:41:17 +0530

    Delete scripts directory

--- scripts/sdload.py
+++
@@ -1,7 +0,0 @@
-from diffusers import StableDiffusionPipeline
-import torch
-
-pipe = StableDiffusionPipeline.from_pretrained(
-    "Lykon/dreamshaper-8",
-    torch_dtype=torch.float16
-).to("cuda")
